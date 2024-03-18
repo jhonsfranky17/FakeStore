6582ead5a192881c5392fb79830b5b1f9898825e
Jen Thomas James <jenthomasjames@gmail.com>
2024-03-18 21:57:04 +0530

    Style: Adjust brand colors

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -2,7 +2,7 @@ const Brand = () => {
   return (
     <div className="flex items-center gap-3">
       <img src="/assets/logo.svg" alt="logo" />
-      <h1 className="text-primary-500 text-lg">Fakestore.io</h1>
+      <h1 className="text-gray-900 text-lg">Fakestore.io</h1>
     </div>
   );
 };
