fafe0cde1aeed20972c4d0cba2b716777faa4224
Jen Thomas James <jenthomasjames@gmail.com>
2024-03-18 21:46:56 +0530

    Feat: Add highlighting of active link in the navbar * Used Navlink component provided by React Router Dom to show the current active route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,36 @@
+import { NavLink } from "react-router-dom";
+
 import Brand from "./Brand";
 import Button from "./Button";
 import IconButton from "./IconButton";
 import { IoSearch, IoPerson, IoBag } from "react-icons/io5";
 
 const Navbar = () => {
-  const NAV_ITEMS = ["Shop All", "New", "Sale", "Reviews", "About Us"];
+  const NAV_ITEMS = [
+    { title: "Products", to: "/products" },
+    { title: "Shop All", to: "/shop-all" },
+    { title: "New", to: "/new" },
+    { title: "Sale", to: "/sale" },
+    { title: "Reviews", to: "/reviews" },
+    { title: "About Us", to: "/about" },
+  ];
 
   return (
     <div className="p-3 flex items-center justify-between border-b">
       <Brand />
       <div className="flex items-center justify-between gap-3">
         {NAV_ITEMS.map((navItem) => (
-          <h2 className="text-slate-500 hover:text-primary-400 cursor-pointer hover:underline">
-            {navItem}
-          </h2>
+          <NavLink
+            key={navItem.to}
+            to={navItem.to}
+            className={({ isActive }) =>
+              isActive
+                ? "text-primary-500 hover:text-primary-600 cursor-pointer"
+                : "text-slate-500 hover:text-primary-400 cursor-pointer hover:underline"
+            }
+          >
+            {navItem.title}
+          </NavLink>
         ))}
       </div>
       <div className="flex items-center">
