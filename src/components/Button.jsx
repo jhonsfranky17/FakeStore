bcca6a25c61435cf67bf0675cddca505c9e0dcdd
Jen Thomas James <jenthomasjames@gmail.com>
2024-03-20 21:33:17 +0530

    Feat: Add startIcon and endIcon props instead of icon prop to control the position of the rendered icon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 
-const Button = ({ children, variant = "contained", icon }) => {
+const Button = ({ children, variant = "contained", startIcon, endIcon }) => {
   const getButtonStyles = () => {
     return clsx("p-3 rounded-md ", {
       "bg-primary-500 text-primary-50 hover:bg-primary-600 active:bg-primary-700":
@@ -15,8 +15,9 @@ const Button = ({ children, variant = "contained", icon }) => {
   return (
     <button className={getButtonStyles()}>
       <div className="flex justify-center items-center gap-3">
-        {icon && icon}
+        {startIcon && startIcon}
         {children}
+        {endIcon && endIcon}
       </div>
     </button>
   );
