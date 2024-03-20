8b15a3734ef47a80df6fb9ca16f5bb498976bb80
Jen Thomas James <jenthomasjames@gmail.com>
2024-03-20 20:18:37 +0530

    Refactor: Change ProductList component name to ProductListPage and adjust imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import ProductList from "./pages/ProductList";
+import ProductListPage from "./pages/ProductListPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/products" element={<ProductList />} />
+      <Route path="/products" element={<ProductListPage />} />
 
       <Route path="*" element={<Navigate to="/products" />} />
     </Routes>
